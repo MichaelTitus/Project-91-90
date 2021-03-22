e9dd47546d3a37fe36e991821e6cbeb40bad3df0
MichaelTitus <76416268+MichaelTitus@users.noreply.github.com>
2021-03-21 20:58:27 -0500

    Update game_login.js

--- game_login.js
+++ game_login.js
@@ -3,5 +3,5 @@ function adduser(){
     var player2name=document.getElementById("playername2input").value;
     localStorage.setItem("playername1", player1name);
     localStorage.setItem("playername2", player2name);
-    window.location="math_page.html";
+    window.location="Math_page.html";
 }
